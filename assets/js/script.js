cc177da84e08786b73fff69f661a2a0fad8b62ec
Yanfang Zhou <zhou7cathy@gmail.com>
2022-12-04 00:10:01 +0800

    add question to javaScrip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 
 var startButton = document.getElementById('start-quiz')
+var questionContainerElement = document.getElementById('question-container')
+var answerElement = document.getElementById('choice-container')
 
 //When you click on Start Quiz button the startGame function will run
 startButton.addEventListener('click', startGame)
 
 function startGame(){
+
+    //Timer
     var timerEl = document.getElementById('countdown');
 
     function countdown() {
@@ -26,24 +30,49 @@ function startGame(){
     }
     countdown();
 
+    //Style of question and answers after click event
     document.getElementById("home").style.display = "none";
     document.getElementById("quiz-container").style.display = "flex";
     document.getElementById("quiz-container").style.flexDirection = "column";
     document.getElementById("choice-container").style.display = "flex";
     document.getElementById("choice-container").style.flexDirection = "column";
+    
+    nextQuestion()
+
+
 }
 
 function nextQuestion(){
-
-
+ 
 }
+
 
 function selectAnswer(){
 
 
 }
 
+var questions = [
+    {
+        question:'Commonly used data types DO Not include:',
+        answer: [
+            {text: 'strings', correct: true},
+            {text: 'booleans', correct: true},
+            {text: 'alerts', correct: false },
+            {text: 'strings', correct: true},
+        ]
+    },
+    {
+        question:'Commonly used data types DO Not include:',
+        answer: [
+            {text: 'strings', correct: true},
+            {text: 'booleans', correct: true},
+            {text: 'alerts', correct: false },
+            {text: 'strings', correct: true},
+        ]
 
+    }
+]
 
 
 
