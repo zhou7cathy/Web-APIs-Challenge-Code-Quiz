2257f8ca5fe8a4c7634a77e3e5dccde086434c68
unknown <zhou7cathy@gmail.com>
2022-12-04 17:39:54 +0800

    add eventListener to each option and go to next question after each click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-
 var startButton = document.getElementById('start-quiz')
-var questionContainerElement = document.getElementById('question-container')
-var answerElement = document.getElementById('choice-container')
+var questionContainer = document.getElementById('question-container')
+var option1 = document.getElementById('option1')
+var option2 = document.getElementById('option2')
+var option3 = document.getElementById('option3')
+var option4 = document.getElementById('option4')
+var index = 0;
 
 //When you click on Start Quiz button the startGame function will run
 startButton.addEventListener('click', startGame)
+//Go to next question after each click
+var nextButton = document.getElementById('option1')
+nextButton.addEventListener('click', nextQuestion)
+var nextButton = document.getElementById('option2')
+nextButton.addEventListener('click', nextQuestion)
+var nextButton = document.getElementById('option3')
+nextButton.addEventListener('click', nextQuestion)
+var nextButton = document.getElementById('option4')
+nextButton.addEventListener('click', nextQuestion)
 
 function startGame(){
 
@@ -12,19 +24,20 @@ function startGame(){
     var timerEl = document.getElementById('countdown');
 
     function countdown() {
-    var timeLeft = 100;
+        var timeLeft = 100;
    
-    var timeInterval = setInterval(function () {
-    timeLeft--;
-    timerEl.textContent = "Time: " + timeLeft;
+        var timeInterval = setInterval(function () {
+            timeLeft--;
+            timerEl.textContent = "Time: " + timeLeft;
 
-    if(timeLeft === 0) {
-      clearInterval(timeInterval);
-      displayMessage();
-    }
+            if(timeLeft === 0) {
+                clearInterval(timeInterval);
+                displayMessage();
+            }
 
-  }, 1000);
+        }, 1000);
     }
+   
     function displayMessage() {
 
     }
@@ -38,12 +51,28 @@ function startGame(){
     document.getElementById("choice-container").style.flexDirection = "column";
     
     nextQuestion()
-
-
+ 
 }
 
+
 function nextQuestion(){
+
+
+    let question1 = questions[index].question;
+    let answer1 = questions[index].answer[0].text;
+    let answer2 = questions[index].answer[1].text;
+    let answer3 = questions[index].answer[2].text;
+    let answer4 = questions[index].answer[3].text;
+
+    questionContainer.innerText = question1;
+    option1.innerText = answer1;
+    option2.innerText = answer2;
+    option3.innerText = answer3;
+    option4.innerText = answer4;
  
+    index++;
+
+    selectAnswer()
 }
 
 
@@ -56,22 +85,50 @@ var questions = [
     {
         question:'Commonly used data types DO Not include:',
         answer: [
-            {text: 'strings', correct: true},
-            {text: 'booleans', correct: true},
-            {text: 'alerts', correct: false },
-            {text: 'strings', correct: true},
+            {text: 'strings', correct: false},
+            {text: 'booleans', correct: false},
+            {text: 'alerts', correct: true },
+            {text: 'strings', correct: false},
         ]
     },
     {
-        question:'Commonly used data types DO Not include:',
+        question:'The condition in an if/else statement is enclosed with____.',
         answer: [
-            {text: 'strings', correct: true},
-            {text: 'booleans', correct: true},
-            {text: 'alerts', correct: false },
-            {text: 'strings', correct: true},
+            {text: 'quote', correct: false},
+            {text: 'curly brackets', correct: false},
+            {text: 'parenthesis', correct: true },
+            {text: 'square brackets', correct: false},
         ]
 
+    },
+    {
+        question:'Array in JavaScript can be used to store____.',
+        answer: [
+            {text: 'numbers and strings', correct: false},
+            {text: 'other arrays', correct: false},
+            {text: 'booleans', correct: false},
+            {text: 'all of the above', correct: true},
+        ]
+    },
+    {
+        question:'Sting value must be enclosed with _____ when being assigned to variables.',
+        answer: [
+            {text: 'commas', correct: false},
+            {text: 'curly brackets', correct: false},
+            {text: 'quotes', correct: true},
+            {text: 'parenthesis', correct: false},
+        ]
+    },
+    {
+        question:'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answer: [
+            {text: 'JavaScript', correct: false},
+            {text: ' terminal/bash', correct: false},
+            {text: 'for loop', correct: false},
+            {text: 'console.log', correct: true},
+        ]
     }
+
 ]
 
 
